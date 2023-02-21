5635317f85c1510c1eef62463987c156fce79224
Paradoxio <89802006+Paradoxio@users.noreply.github.com>
2023-02-21 14:37:17 +0200

    Update F104834_T1.py

--- F104834_T1.py
+++ F104834_T1.py
@@ -3,3 +3,15 @@
 #"no real roots" - когато няма реални корени
 #"x1" - когато има един корен, където x1 е стойността му
 #"special case" - когато е особен случай
+#https://www.geeksforgeeks.org/python-program-to-solve-quadratic-equation/
+import math
+
+#дефиниция
+def eq_Roots(a,b,c):
+
+    #смятан на дискриминанта
+        dis = b * b - 4 * a * c
+        sqrt_value = math.sqrt(abs(dis))
+
+    #проверка за два или ени корен
+    print((-b + sqrt_value)/(2 * a))
