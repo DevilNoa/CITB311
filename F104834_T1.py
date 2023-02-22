263846514ebbb6748b88251c2c2b87ecab7a710b
Paradoxio <89802006+Paradoxio@users.noreply.github.com>
2023-02-22 12:17:20 +0200

    Update F104834_T1.py

--- F104834_T1.py
+++ F104834_T1.py
@@ -1,17 +1,27 @@
 #програма която решава квадратно уравнение с коефициенти който са като аргументи
-#"x1|x2" - когато има два реални корена, където x1 и x2 са стойностите им
-#"no real roots" - когато няма реални корени
-#"x1" - когато има един корен, където x1 е стойността му
-#"special case" - когато е особен случай
-#https://www.geeksforgeeks.org/python-program-to-solve-quadratic-equation/
+#"x1|x2" - когато има два реални корена, където x1 и x2 са стойностите им - готово
+#"no real roots" - когато няма реални корени - готово
+#"x1" - когато има един корен, където x1 е стойността му - готово
+#"special case" - когато е особен случай-готово
 import math
-
-#дефиниция
-def eq_Roots(a,b,c):
-
-    #смятан на дискриминанта
-        dis = b * b - 4 * a * c
-        sqrt_value = math.sqrt(abs(dis))
-
-    #проверка за два или ени корен
-    print((-b + sqrt_value)/(2 * a))
+    #функция за намиране на корените
+def eq_Roots(a, b, c):
+    # смятане на дискриминантата
+    dis = ( b ** 2 ) - ( 4 * a * c )
+    sqrt_val = math.sqrt(abs(dis))
+    # решаваме и принтираме
+    if dis > 0:
+        print((-b + sqrt_val) / (2 * a),"|",((-b - sqrt_val) / (2 * a)))
+    elif dis == 0:
+        print(-b / (2 * a))
+    else:
+        print("no real roots")
+# Driver Program
+a = 1
+b = 10
+c = -24
+# If a is 0, then incorrect equation
+if a == 0:
+    print("special case")
+else:
+    eq_Roots(a, b, c)
