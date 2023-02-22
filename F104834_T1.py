237401eb4bba5f2083fa049c943a5df5ccbc3e96
Paradoxio <89802006+Paradoxio@users.noreply.github.com>
2023-02-22 12:38:56 +0200

    Update F104834_T1.py

--- F104834_T1.py
+++ F104834_T1.py
@@ -4,24 +4,33 @@
 #"x1" - когато има един корен, където x1 е стойността му - готово
 #"special case" - когато е особен случай-готово
 import math
+
+
     #функция за намиране на корените
 def eq_Roots(a, b, c):
     # смятане на дискриминантата
     dis = ( b ** 2 ) - ( 4 * a * c )
     sqrt_val = math.sqrt(abs(dis))
+
     # решаваме и принтираме
     if dis > 0:
         print((-b + sqrt_val) / (2 * a),"|",((-b - sqrt_val) / (2 * a)))
+
     elif dis == 0:
         print(-b / (2 * a))
+
     else:
         print("no real roots")
+
+
 # Driver Program
-a = 1
-b = 10
-c = -24
+a = int(input())
+b = int(input())
+c = int(input())
+
 # If a is 0, then incorrect equation
 if a == 0:
     print("special case")
+
 else:
     eq_Roots(a, b, c)
