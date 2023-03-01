a184f4afb41775c636dfb8d7b11d97f44dfa7713
Paradoxio <89802006+Paradoxio@users.noreply.github.com>
2023-03-01 12:14:05 +0200

    Add files via upload

---
+++ lectures/main.py
@@ -0,0 +1,22 @@
+# print("Hello World") # ред 1
+
+# 4/8 ред
+# print(2+2)
+# print(34287+2348972)
+# print(1/2)
+# print(1//2)
+# print(1.0//2.0)
+
+# print(1000000000000000000 / 2)  # ред 10
+
+# print(0xAF)  # ред 12
+
+# print(0o10)  # ред 14
+
+# x = 3
+# x = x * 2
+# print(x)  # 16/18
+
+# input("the meaning of life")
+#  # чете от конолата след текста
+# print("But what is the question then ")
